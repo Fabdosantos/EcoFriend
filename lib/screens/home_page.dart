5491112fa3c82bed33046d841611ae68c94269d5
Fabdosantos <fabdosantos2009@hotmail.com>
2023-10-18 20:25:50 -0300

    Cria botoes de interação

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:ecofriend/widgets/bottom_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,17 +6,32 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text("Eco Friend", style: TextStyle()),
-        TextButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text("Voltar"),
-        )
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Eco Friend"),
+        centerTitle: true,
+        backgroundColor: Colors.yellow,
+      ),
+      body: const SizedBox(
+        width: double.maxFinite,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Bem-vindo!",
+              style: TextStyle(
+                  fontSize: 24, decorationStyle: TextDecorationStyle.solid),
+            ),
+            Card(
+              child: Text(
+                "Card",
+                style: TextStyle(fontSize: 24),
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomBar(),
     );
   }
 }
---
+++ lib/widgets/bottom_bar.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({super.key});
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  final List<bool> _isActiveList = [true, false, false];
+
+  void _handleButtonActived(int index) {
+    setState(() {
+      for (int i = 0; i < _isActiveList.length; i++) {
+        _isActiveList[i] = i == index;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.bottomCenter,
+      height: 70,
+      color: const Color.fromARGB(186, 229, 229, 229),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Column(
+            children: [
+              IconButton(
+                color: _isActiveList[0] ? Colors.green : Colors.grey,
+                icon: const Icon(
+                  Icons.home,
+                  size: 34,
+                ),
+                onPressed: () => _handleButtonActived(0),
+              ),
+              const Text("Home")
+            ],
+          ),
+          Column(
+            children: [
+              IconButton(
+                color: _isActiveList[1] ? Colors.green : Colors.grey,
+                icon: const Icon(
+                  Icons.person,
+                  size: 34,
+                ),
+                onPressed: () => _handleButtonActived(1),
+              ),
+              const Text("Usuario")
+            ],
+          ),
+          Column(
+            children: [
+              IconButton(
+                color: _isActiveList[2] ? Colors.green : Colors.grey,
+                icon: const Icon(
+                  Icons.add_to_drive_sharp,
+                  size: 34,
+                ),
+                onPressed: () => _handleButtonActived(2),
+              ),
+              const Text("Recicle")
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
